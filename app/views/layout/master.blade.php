f92db34060e17a8247d244ac20a2a65d9f180389
Simon Bennett <simon@bennett.im>
2014-06-18 22:38:03 +0100

    Created Master Layout

---
+++ app/views/layout/master.blade.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>HireIt</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Bootstrap core CSS -->
+    <link href="http://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.1.0/css/bootstrap.css" rel="stylesheet"
+          media="screen">
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7/html5shiv.js"></script>
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/respond.js/1.3.0/respond.js"></script>
+    <![endif]-->
+    <link href="http://fonts.googleapis.com/css?family=Crimson+Text" rel="stylesheet" type="text/css">
+    <link href="http://fonts.googleapis.com/css?family=Allerta" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="/css/style.css"/>
+</head>
+<style>
+    .container {
+        margin-top: 100px;
+    }
+</style>
+<body>
+
+<div class="container">
+
+
+    <div class="row">
+        <div class="col-md-8"><img src="http://www.phoenixfleet.com/images/p21-3lge.jpg" alt="" class="col-md-8"/></div>
+        <div class="col-md-4">
+            <form action="/search" method="post">
+                I want to hire a boat on the
+                <div class="input-group">
+                    <span class="input-group-addon"><span class="glyphicon glyphicon-calendar"></span>
+</span>
+                    <input type="text" class="form-control datepicker" placeholder="Date" name="date">
+                </div>
+                <div class="input-group">
+
+</span>
+                    <select class="form-control">
+                        <option>1</option>
+                        <option>2</option>
+                        <option>3</option>
+                        <option>4</option>
+                        <option>5</option>
+                    </select>
+                    <span class="input-group-addon" name="hours">hours</span>
+
+                </div>
+                <div class="form-group">
+                    <div class=" col-sm-10">
+                        <button type="submit" class="btn btn-default">Search</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+
+
+</div>
+<!-- /container -->
+
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="http://cdnjs.cloudflare.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="http://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.1.0/js/bootstrap.min.js"></script>
+
+<script>
+
+
+</script>
+
+</body>
+</html>
