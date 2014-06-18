69cf5f3d4773c1d276e778b8640e19fa5d2772e5
Simon Bennett <simon@bennett.im>
2014-06-18 22:38:40 +0100

    Home Controller Loads View Template
    Still messes about with the actions need to sort them all out

--- app/HireIt/Controllers/HomeController.php
+++ app/HireIt/Controllers/HomeController.php
@@ -3,19 +3,34 @@ namespace HireIt\Controllers;
 
 
 use HireIt\Actions\Home\LoadHomePage;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\Request;
+use Illuminate\View\Factory;
 use Mrsimonbennett\Transport\Callback;
 
 class HomeController extends BaseController  {
+    /**
+     * @var \Illuminate\View\Factory
+     */
+    private $view;
+
+    /**
+     * @param Factory $factory
+     */
+    public function __construct(Factory $factory)
+    {
+        $this->view = $factory;
+    }
     public function getIndex(Request $request,LoadHomePage $loadHomePage)
     {
-        $loadHomePage->setCallBack(new Callback($this,'getIndexSuccess','getIndexFailure'));
-        return $loadHomePage->fire();
+        return $loadHomePage->setCallBack(new Callback($this,'getIndexSuccess','getIndexFailure'))->fire();
 
     }
-    public function getIndexSuccess()
+    public function getIndexSuccess(Collection $collection)
     {
-        return 'hello';
+       return $this->view->make('layout.master');
+        return $collection;
+
     }
     public function getIndexFailure()
     {
