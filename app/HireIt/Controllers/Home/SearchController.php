cbcb14d639c0f76998319e8562b418195cef0de0
Simon Bennett <simon@bennett.im>
2014-06-24 08:21:53 +0100

    Make the Search Controller more realistic

--- app/HireIt/Controllers/Home/SearchController.php
+++ app/HireIt/Controllers/Home/SearchController.php
@@ -3,20 +3,23 @@
 
 namespace HireIt\Controllers\Home;
 
-
+use Carbon\Carbon;
 use HireIt\Commands\Search\SearchForBookingsCommand;
 use HireIt\Controllers\BaseController;
 use Mrsimonbennett\Transport\Callback;
 use Illuminate\Http\Request;
 
-class SearchController extends BaseController{
+class SearchController extends BaseController
+{
     public function postIndex(Request $request)
     {
-        $callback = new Callback($this,'postIndexSuccess','postIndexFail');
-        $command = new SearchForBookingsCommand($request->only(['date']));
-        $this->commandBus->setCallback($callback)->execute($command);
+        $callback = new Callback($this, 'postIndexSuccess', 'postIndexFail');
+        $date = new Carbon($request->input('date'));
+        $command = new SearchForBookingsCommand($date);
 
+        $this->commandBus->setCallback($callback)->execute($command);
     }
+
     public function postIndexSuccess()
     {
         var_dump(func_get_args());
